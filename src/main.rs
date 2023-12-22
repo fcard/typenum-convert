608f971538943330f676abafc51985ddf64e2ad4
phi <ficarde@gmail.com>
2023-12-22 17:37:48 -0300

    add readme example to main function

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 pub mod conv;
 pub use conv::{T8, T16, T32, T64};
 
+pub mod to_uint;
+
 pub(crate) mod test;
 
 #[cfg(feature = "norm")]
@@ -12,4 +14,10 @@ pub(crate) mod norm;
 fn main() {
   test::main1();
   test::main2();
+  use typenum::{U4, Unsigned};
+  type Four   = U4;
+  type Eight  = T32<{Four::U32 * 2}>;
+  type Twelve = T32<{Four::U32 + Eight::U32}>;
+  println!("{} {} {}",
+    Four::U32, Eight::U32, Twelve::to_u32());
 }
